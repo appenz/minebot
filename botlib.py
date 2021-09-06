c84f86f7378bae8db88d70459707981ea33ca4b1
Guido Appenzeller <guido@appenzeller.net>
2021-09-05 21:37:37 -0700

    Initial checkin of build logic. Can build a sophisticated sorting system

---
+++ botlib.py
@@ -0,0 +1,37 @@
+#
+# Utility functions that didn't fit anywhere else
+#
+
+import time
+import datetime
+
+activity_start = 0
+activity_name = "None"
+
+def myTime():
+	now = datetime.datetime.now()
+	return now.strftime("%H:%M:%S")
+
+
+def startActivity(bot,name):
+	global activity_start
+	global activity_name
+
+	t_str = myTime()
+	print(35*'-')
+	print(f'   {name:20} ({t_str})')
+	print(35*'-')
+	activity_start = time.time()
+	activity_name = name
+	bot.stopActivity = False
+
+def stopActivity(bot):
+	global activity_start
+	global activity_name
+
+	t_str = myTime()
+	d_str = str(datetime.timedelta(seconds=int(time.time()-activity_start)))
+	print(f'Activity {activity_name:15} ended at {t_str} (duration: {d_str})')
+	print('')
+	bot.clearControlStates('sneak', False)
+	bot.stopActivity = True
---
+++ build.py
@@ -0,0 +1,20 @@
+#
+# Build various blueprints
+#
+
+from javascript import require
+Vec3     = require('vec3').Vec3
+
+from equipment import *
+from blocks import *
+from farming import *
+
+sorterParts= {
+  "Redstone Comparator":3,
+  "Redstone Torch":3,
+  "Redstone Repeater":3,
+  "Stone Bricks":26,
+  "Chest": 7,
+  "Redstone Dust" : 9,
+  "Hopper" : 10,
+}
